ddbafbbde0ff31e89775566fc02b0e17342843ac
Daniel Riley <driley3@gmail.com>
2016-09-28 01:33:58 -0400

    npm installed react-redux-starter-kit and installed material-ui

---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { IndexLink, Link } from 'react-router'
+import './Header.scss'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
+
+export const Header = () => (
+  <AppBar
+    title="Title"
+    iconElementRight={
+      <IconMenu
+        iconButtonElement={
+          <IconButton><MoreVertIcon /></IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <MenuItem primaryText="Refresh" />
+        <MenuItem primaryText="Help" />
+        <MenuItem primaryText="Sign out" />
+      </IconMenu>
+    }
+  />
+)
+
+export default Header
---
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../../components/Header'
+import './CoreLayout.scss'
+import '../../styles/core.scss'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+
+export const CoreLayout = ({ children }) => (
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <div>
+      <Header />
+      <div className='container-fluid text-center'>
+        <div className='core-layout__viewport'>
+          {children}
+        </div>
+      </div>
+    </div>
+  </MuiThemeProvider>
+)
+
+CoreLayout.propTypes = {
+  children : React.PropTypes.element.isRequired
+}
+
+export default CoreLayout
